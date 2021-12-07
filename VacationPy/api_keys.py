06f3ded248e5d3cb309dcd1e603da6a63f92fc0c
Prince Emenalo <pemenalo@gmail.com>
2021-12-06 19:37:01 -0500

    commit to main

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -3,7 +3,3 @@ weather_api_key = "1ceba8138809e05a69989d6457efd9ba"
 
 # Google API Key
 g_key = "AIzaSyAq29PAqJa7Ui-y32jqwrzVHbDoiGGEpjA"
-
-
-# {api_key}
-{api_key} = "1ceba8138809e05a69989d6457efd9ba"
